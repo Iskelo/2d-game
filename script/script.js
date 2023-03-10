8b080f5c56f8c85f74bc71dc8f3b5451533aeaef
Iskelo <iskelo.kap@gmail.com>
2023-03-10 11:10:45 +0100

    rename image

---
+++ script/script.js
@@ -0,0 +1,12 @@
+window.addEventListener('load', function () {
+	const canvas = document.getElementById('myCanvas');
+	const ctx = canvas.getContext('2d');
+	canvas.width = 500;
+	canvas.height = 500;
+
+	class InputHandler { }
+	class Projectile { }
+	class Particle { }
+	class Player { }
+
+})
