e9e47d55bfef76f20f146b90ebff41214480a8d7
Iskelo <iskelo.kap@gmail.com>
2023-03-10 11:22:53 +0100

    build game

--- script/script.js
+++ script/script.js
@@ -7,6 +7,31 @@ window.addEventListener('load', function () {
 	class InputHandler { }
 	class Projectile { }
 	class Particle { }
-	class Player { }
+	class Player {
+		constructor(game) {
+			this.game = game;
+			this.width = 155;
+			this.height = 100;
+			this.x = 20;
+			this.y = 100;
+			this.speedY = 0;
+		}
+		update() {
+			this.y += this.speedY;
+		}
+		draw(context) {
+			context.fillRect(this.x, this.y, this.width, this.height);
+		}
+	}
+	class Enemy { }
+	class Layer { }
+	class Background { }
+	class UI { }
+	class Game {
+		constructor(width, height) {
+			this.width = width;
+			this.height = height;
+		}
+	}
 
 })
